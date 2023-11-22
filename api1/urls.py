d9b23d5699d49872782b62f2155993ed3d140e65
rakeshnayal-k <rakeshdravid25@gmail.com>
2023-11-22 18:48:08 +0530

    added api1 with @api_view()

---
+++ api1/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import hello_world
+
+urlpatterns = [
+    path('hello/', hello_world),
+]
---
+++ api1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# Create your views here.
+@api_view()
+def hello_world(request):
+    return Response({'msg':'Hello World'})
