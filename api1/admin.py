01b7955ec6861151776515e28b03aed5cb2124e1
rakeshnayal-k <rakeshdravid25@gmail.com>
2023-11-22 23:47:47 +0530

    crud api with @api_view()

--- api1/admin.py
+++ api1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Students
 
 # Register your models here.
+@admin.register(Students)
+class StudentsAdmin(admin.ModelAdmin):
+    list_display = ['id','roll','name','city']
+    ordering = ['id']
--- api1/views.py
+++ api1/views.py
@@ -1,9 +1,55 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import StudentSerializer
+from .models import Students
+from rest_framework import status
 
 
 # Create your views here.
-@api_view()
-def hello_world(request):
-    return Response({'msg':'Hello World'})
+# @api_view()
+# def hello_world(request):
+#     return Response({'msg':'Hello World'})
+
+@api_view(['POST','GET','PUT','DELETE'])
+def students(request):
+    if request.method == 'GET':
+        try:
+            in_id = request.data.get('id',None)
+            if in_id is not None:
+                stu = Students.objects.get(id=in_id)
+                serializer = StudentSerializer(stu)
+                return Response({'Students':serializer.data})
+            stu = Students.objects.all()
+            serializer = StudentSerializer(stu, many=True)
+            return Response({'Students':serializer.data})
+        except Exception as e:
+            return Response({'error msg':str(e)})
+    if request.method == 'POST':
+        try:
+            serializer = StudentSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'msg':'Student Created'}, status=status.HTTP_201_CREATED)
+            return Response({'error msg':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error msg':str(e)})
+    if request.method == 'PUT':
+        try:
+            id = request.data.get('id',None)
+            stu = Students.objects.get(pk=id)
+            serializer = StudentSerializer(stu, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'msg':'Student Updated'},status=status.HTTP_201_CREATED)
+            return Response({'error msg':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error msg':str(e)})
+    if request.method == 'DELETE':
+        try:
+            id = request.data.get('id',None)
+            stu = Students.objects.get(pk=id)
+            stu.delete()
+            return Response({'msg':'Student deleted'})
+        except Exception as e:
+            return Response({'error msg':str(e)})
